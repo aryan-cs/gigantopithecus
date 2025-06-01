014dcbc71f5911925b0fc7eb50a517bb37be42d4
aryan-cs <aryan05g@gmail.com>
2025-05-31 19:04:24 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import ollama
+
+MODEL_NAME = "gemma3:1b"
+
+def main():
+    print(f"Now chatting with {MODEL_NAME}")
+    conversation = []
+
+    while True:
+        user_input = input("> ")
+        if user_input == "/bye":
+            break
+
+        conversation.append({'role': 'user', 'content': user_input})
+        print('', end='', flush=True)
+
+        reply = ''
+        for chunk in ollama.chat(model=MODEL_NAME, messages=conversation, stream=True):
+            content = chunk['message']['content']
+            print(content, end='', flush=True)
+            reply += content
+
+        print()
+        conversation.append({'role': 'assistant', 'content': reply})
+
+if __name__ == "__main__":
+    main()
